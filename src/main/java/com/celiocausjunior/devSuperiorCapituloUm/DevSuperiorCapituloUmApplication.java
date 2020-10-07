3c9a87d9850224d24d499957cf262552044e167b
Celio Caus Junior <celiocausjunior@gmail.com>
2020-10-07 16:12:58 -0300

    Projeto Criado, Client Entity

---
+++ src/main/java/com/celiocausjunior/devSuperiorCapituloUm/DevSuperiorCapituloUmApplication.java
@@ -0,0 +1,13 @@
+package com.celiocausjunior.devSuperiorCapituloUm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSuperiorCapituloUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSuperiorCapituloUmApplication.class, args);
+	}
+
+}
