3d8839c7c30c04c4c830179fd080f3cb175ad03b
Celio Caus Junior <celiocausjunior@gmail.com>
2020-10-08 13:12:12 -0300

    Find by Id, Service and Resorce Exceptions

--- src/main/java/com/celiocausjunior/devSuperiorCapituloUm/services/ClientService.java
+++ src/main/java/com/celiocausjunior/devSuperiorCapituloUm/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.celiocausjunior.devSuperiorCapituloUm.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.celiocausjunior.devSuperiorCapituloUm.dtos.ClientDTO;
 import com.celiocausjunior.devSuperiorCapituloUm.entities.Client;
 import com.celiocausjunior.devSuperiorCapituloUm.repositories.ClientRepository;
+import com.celiocausjunior.devSuperiorCapituloUm.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,5 +23,12 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest page) {
 		Page<Client> list = clientRepository.findAll(page);
 		return list.map(x -> new ClientDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = clientRepository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not Found"));
+		return new ClientDTO(entity);
 	}
 }
